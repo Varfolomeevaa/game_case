3dcd9b0b7fa060c8050dd34a77758211f9c07da0
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-05-12 23:30:20 +0700

    r8

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ Sineokaya Anastasia
 from time import sleep
 import RU_LOCAL as ru
 import random
-
 k = 0
 b = []
-while k != 3:
+print(b)
+c = ''
+while k != 4:
+    if 'сходить в библиотеку' in b:
+        c = '-сходить в аптеку'
     print('''Вы можете…
 -сходить в библиотеку
 -сходить в кинотеатр
 -сходить в университет''')
+    print(c)
     a = input('Ваш выбор:')
     if a in b:
         print('Вы там уже были! Выберете другую локацию')
@@ -23,7 +27,6 @@ while k != 3:
             k += 1
             b.append(a)
             print('да1')
-
         elif a == 'сходить в кинотеатр':
             k += 1
             b.append(a)
@@ -32,3 +35,7 @@ while k != 3:
             k += 1
             b.append(a)
             print('да3')
+        elif a == 'сходить в аптеку':
+            k += 1
+            b.append(a)
+            print('да4')
