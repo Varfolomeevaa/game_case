a40ddf258b04f5f6cabeadfdc3f34bfef612aefd
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-05-13 00:51:48 +0700

    library

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ Group:
 Varfolomeeva Viktoria
 Sineokaya Anastasia
 """
-
-
 from time import sleep
 import RU_LOCAL as RU
 import random
+
+score = 0
 
 class color:
    PURPLE = '\033[95m'
@@ -20,6 +20,67 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+
+def library():
+    global score
+    flag = 0
+    print(color.BOLD + RU.library_1)
+    sleep(1)
+    print(color.BOLD + RU.library_2)
+    sleep(1)
+    print(color.BOLD + RU.library_3)
+    sleep(1)
+    print(color.BOLD + RU.library_4)
+    sleep(1)
+    print(color.BOLD + RU.library_q_1)
+    sleep(1)
+    print(color.BOLD + RU.library_q_2)
+    sleep(1)
+    ans_1 = int(input(color.BOLD + RU.library_q))
+    if ans_1 == 1:
+        print(color.BOLD + RU.library_q_1[2:])
+        sleep(1)
+        print(color.BOLD + RU.library_a_1)
+        sleep(1)
+        ans_2 = input(color.BOLD + RU.library_q_3).lower()
+        if ans_2 == RU.library_yes:
+            flag = 1
+            print(color.BOLD + RU.library_q_2[2:])
+            sleep(1)
+            print(color.BOLD + RU.library_a_2_1)
+            sleep(1)
+            print(color.BOLD + RU.library_5)
+            sleep(1)
+            print(color.BOLD + RU.library_a_2_2)
+    else:
+        flag = 1
+        print(color.BOLD + RU.library_q_2[2:])
+        sleep(1)
+        print(color.BOLD + RU.library_a_2_1)
+        sleep(1)
+        print(color.BOLD + RU.library_5)
+        sleep(1)
+        print(color.BOLD + RU.library_a_2_2)
+        sleep(1)
+        ans_2 = input(color.BOLD + RU.library_q_3).lower()
+        if ans_2 == RU.library_yes:
+            print(color.BOLD + RU.library_q_1[2:])
+            sleep(1)
+            print(color.BOLD + RU.library_a_1)
+    print(color.BOLD + RU.library_6)
+    sleep(1)
+    print(color.BOLD + RU.library_7)
+    sleep(1)
+    if flag == 1:
+        print(color.BOLD + RU.library_8)
+        score += 10
+    print(color.BOLD + RU.library_9, score)
+
+library()
+
+
+
+
 
 print(color.BOLD + RU.phrase_1)
 sleep(1)
@@ -37,6 +98,7 @@ print(color.BOLD + RU.phrase_7)
 sleep(4)
 print(color.BOLD + RU.phrase_8)
 sleep(4)
+
 
 k = 0
 b = []
