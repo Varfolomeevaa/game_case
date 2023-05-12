00b0e0ff3da35d02b2e800f95440deac640eea5d
sineokayaa <a.sineokaya@g.nsu.ru>
2023-05-12 23:42:37 +0700

    m6

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ Sineokaya Anastasia
 """
 
 from time import sleep
-import RU_LOCAL as ru
+
+import RU_LOCAL as RU
 import random
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+print(color.BOLD + RU.phrase1)
+
 k = 0
 b = []
-print(a)
+print(k)
 c = ''
 while k != 4:
     if 'сходить в библиотеку' in b:
